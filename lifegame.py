47262b8ac8479b0faf0f5119228fd00b8a0c4bb7
circulene <circulene@gmail.com>
2017-04-13 22:04:40 +0900

    First commit

---
+++ lifegame.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.graphics import Color
+from kivy.graphics import Rectangle
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+from lifegame_core import Cells
+
+class LifegameWidget(Widget):
+    def update(self, cells):
+        self.canvas.clear()
+        for y in range(cells.ny):
+            for x in range(cells.nx):
+                status = cells.cell(x, y)
+                if status == Cells.ALIVE:
+                    with self.canvas:
+                        unit = 10
+                        scale = (cells.nx/self.canvas.size[0], cells.ny/self.canvas.size[1])
+                        Color(1, 0, 0)
+                        Rectangle(pos=(scale[0]*x*unit, scale[1]*y*unit), size=unit*scale)
+
+class LifegameApp(App):
+    def build(self):
+        self.cells = Cells(nx, ny)
+        self.title = "Lifegame"
+        self.gen = 0
+        self.console = LifegameConsole()
+        root = BoxLayout(orientation='vertical')
+        self.label = label = Label(text='0')
+        self.wid = wid = LifegameWidget()
+        Clock.schedule_interval(self.update, period)
+        layout = BoxLayout(size_hint=(1, None), height=50)
+        root.add_widget(layout)
+        root.add_widget(label)
+        root.add_widget(wid)
+        return root
+
+    def update(self, dt):
+        self.cells.survive()
+        self.wid.update(self.cells)
+        self.console.write(gen, self.cells)
+        self.gen += 1
+
+class LifegameConsole:
+    DISPLAY_STATUS = {Cells.UNDEFINED: "?",
+                      Cells.DEAD: ".",
+                      Cells.ALIVE: "@"}
+
+    def write(self, gen, cells):
+#        for i in range(self.cells.ny+1):
+#            print("", end="\r")
+        print("gen={0}".format(gen))
+        for y in range(cells.ny):
+            for x in range(cells.nx):
+                status = cells.cell(x, y)
+                print("{0} ".format(LifegameConsole.DISPLAY_STATUS[status]), end="")
+            print("")
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("usage: {0} nx ny".format(sys.argv[0]))
+        quit()
+    nx = int(sys.argv[1])
+    ny = int(sys.argv[2])
+    period = 0.5
+    LifegameApp().run()
+
---
+++ lifegame_core.py
@@ -0,0 +1,43 @@
+import random
+import time
+
+class Cells:
+    UNDEFINED = 0
+    DEAD = 1
+    ALIVE = 2
+
+    def __init__(self, nx, ny):
+        self.nx = nx
+        self.ny = ny
+        self._cells = [[Cells.UNDEFINED for y in range(ny)] for x in range(nx)]
+        self._nextCells = [[Cells.UNDEFINED for y in range(ny)] for x in range(nx)]
+        for x in range(nx):
+            for y in range(ny):
+                status = random.choice((Cells.DEAD, Cells.ALIVE))
+                self._cells[x][y] = status
+
+    def cell(self, x, y):
+        return self._cells[x % self.nx][y % self.ny]
+
+    def _countAliveNeighbours(self, x, y):
+        aliveNeighbours = 0
+        neighbours = ((-1, -1), (0, -1), ( 1, -1),
+                      (-1,  0),          ( 1,  0),
+                      (-1,  1), (0,  1), ( 1,  1))
+        for (ix, iy) in neighbours:
+            neighbour = self.cell(x + ix, y + iy)
+            if neighbour == Cells.ALIVE:
+                aliveNeighbours += 1
+        return aliveNeighbours
+
+    def survive(self):
+        for x in range(self.nx):
+            for y in range(self.ny):
+                aliveNeighbours = self._countAliveNeighbours(x, y)
+                self._nextCells[x][y] = self._cells[x][y]
+                if self._cells[x][y] == Cells.DEAD and aliveNeighbours == 3:
+                    self._nextCells[x][y] = Cells.ALIVE
+                if self._cells[x][y] == Cells.ALIVE and (aliveNeighbours <= 1 or aliveNeighbours >= 4):
+                    self._nextCells[x][y] = Cells.DEAD
+        self._cells = self._nextCells
+
