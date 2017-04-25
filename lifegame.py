4b02e3bef9e88747f15968551014aa1f78c1a2a6
circulene <circulene@gmail.com>
2017-04-26 08:33:28 +0900

    Allow to configure parameters on GUI

--- lifegame.py
+++ lifegame.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 import time
 
@@ -7,68 +8,105 @@ from kivy.app import App
 from kivy.clock import Clock
 from kivy.graphics import Color
 from kivy.graphics import Rectangle
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+from kivy.properties import ListProperty
+from kivy.properties import StringProperty
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 
 from lifegame_core import Cells
 
 class LifegameWidget(Widget):
-    def update(self, cells):
+    def _cellColor(self, gen):
+        intensity = gen/100 + 0.3
+        r = 1 if intensity > 1 else intensity
+        return r
+
+    def updateCanvas(self, cells):
+        print('start updating')
         self.canvas.clear()
-        for y in range(cells.ny):
-            for x in range(cells.nx):
-                status = cells.cell(x, y)
-                if status == Cells.ALIVE:
+        for x in range(cells.nx):
+            for y in range(cells.ny):
+                if cells.cell(x,y) == Cells.ALIVE:
                     with self.canvas:
-                        csize = (self.size[0]/cells.nx, self.size[1]/cells.ny)
-                        intensity = cells.gen(x, y)/100 + 0.3 
-                        r = 1 if intensity > 1 else intensity
-                        Color(r, 0, 0)
-                        Rectangle(pos=(csize[0]*x, csize[1]*y), size=csize)
+                        cs = (self.size[0]/cells.nx, self.size[1]/cells.ny)
+                        Color(self._cellColor(cells.gen(x, y)), 0, 0)
+                        Rectangle(size=cs, pos=(cs[0]*x + self.x, cs[1]*y + self.y))
+        print('end updating')
+
+class IntegerInput(TextInput):
+    pattern = re.compile('[0-9]')
+
+    def insert_text(self, substring, from_undo=False):
+        s = ''
+        for ch in substring:
+            if self.pattern.match(ch):
+                s += ch
+        return super(IntegerInput, self).insert_text(s, from_undo=from_undo)
+
+class LifegameRoot(Widget):
+    triggerLabel = StringProperty('start')
+
+    def __init__(self, **kwargs):
+        super(LifegameRoot, self).__init__(**kwargs)
+        self.isRunning = False
+        self.isInitialized = False
+        self.cells = None
+        self.period = 0.5
+        self.schedule = None
+        self.ids['sizeX'].text = '100'
+        self.ids['sizeY'].text = '100'
+        self.ids['density'].text = '5'
+
+    def initCells(self, force=False):
+        if self.isInitialized == True and force == False:
+            print('skip initializing')
+            return
+        nx = int(self.ids['sizeX'].text)
+        ny = int(self.ids['sizeY'].text)
+        density = int(self.ids['density'].text)
+        print('nx={0} ny={1} density={2}'.format(nx, ny, density))
+        self.cells = Cells(nx, ny, density)
+        self.isInitialized = True
+
+    def startCells(self):
+        self.schedule = Clock.schedule_interval(self.updateCells, self.period)
+
+    def stopCells(self):
+        self.schedule.cancel()
+
+    def updateCells(self, dt):
+        start = time.time()
+        self.cells.survive()
+        now = time.time()
+        elapsed = (now - start) * 1000
+        print('elapsed={0}'.format(elapsed))
+        self.ids['widget'].updateCanvas(self.cells)
+        print('refreshed')
+
+    def onPressTrigger(self):
+        if self.isRunning:
+            self.triggerLabel = 'start'
+            self.isRunning = False
+            self.stopCells()
+        else:
+            self.triggerLabel = 'stop'
+            self.isRunning = True
+            self.initCells()
+            self.startCells()
+
+    def onPressReset(self):
+        if self.isRunning == False:
+            self.ids['widget'].canvas.clear()
+            self.initCells(force=True)
 
 class LifegameApp(App):
-    def __init__(self, nx, ny, density, **kwargs):
+    def __init__(self, **kwargs):
         super(LifegameApp, self).__init__(**kwargs)
-        self.cells = Cells(nx, ny, density)
-        self.title = "Lifegame"
-        self.gen = 0
+        self.title = 'Lifegame'
 
     def build(self):
-        root = BoxLayout(orientation='vertical')
-        self.label = label = Label(text='0', size_hint=(None, 0.1))
-        self.wid = wid = LifegameWidget()
-        Clock.schedule_interval(self.update, period)
-        root.add_widget(label)
-        root.add_widget(wid)
-        return root
-
-    def update(self, dt):
-        self.cells.survive()
-        self.wid.update(self.cells)
-        self.label.text = "gen={0}".format(self.gen)
-        self.gen += 1
-
-class LifegameConsole:
-    DISPLAY_STATUS = {Cells.UNDEFINED: "?",
-                      Cells.DEAD: ".",
-                      Cells.ALIVE: "@"}
-
-    def write(self, gen, cells):
-        print("gen={0}".format(gen))
-        for y in range(cells.ny):
-            for x in range(cells.nx):
-                status = cells.cell(x, y)
-                print("{0} ".format(LifegameConsole.DISPLAY_STATUS[status]), end="")
-            print("")
+        return LifegameRoot()
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("usage: {0} nx ny [density]".format(sys.argv[0]))
-        quit()
-    nx = int(sys.argv[1])
-    ny = int(sys.argv[2])
-    density = int(sys.argv[3])
-    period = 0.5
-    LifegameApp(nx, ny, density).run()
+    LifegameApp().run()
 
